008d156f4eca37e0aefea5a78b6acde5aa3b3614
rajkiran111 <rajkiran1230@gmail.com>
2019-01-15 14:27:48 +0530

    Update havoc_riva.mk

--- havoc_riva.mk
+++ havoc_riva.mk
@@ -18,10 +18,10 @@ DEVICE_PATH := device/xiaomi/riva
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 
 # Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/havoc/config/common_full_phone.mk)
+$(call inherit-product, vendor/havoc/config/common.mk)
 
 # Inherit from riva device
 $(call inherit-product, device/xiaomi/riva/device.mk)
